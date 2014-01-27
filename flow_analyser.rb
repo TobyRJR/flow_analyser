600b39837ff2e1433a9ad72b437fb15be38f133c
TobyRJR <toby@dante.net>
2014-01-27 01:00:45 +0000

    end of day2

--- flow_analyser.rb
+++ flow_analyser.rb
@@ -1,11 +1,11 @@
-Traffic_units= { :k => 10^3, :M => 10^6, :G => 10^9 }
+Traffic_units= { :k => 1000, :M => 1000000, :G => 1000000000 }
 
 LOOKUP_PREFIX = "dig +short "
 LOOKUP_SUFFIX = ".origin.asn.cymru.com TXT"
 AS_LOOKUP_SUFFIX = ".asn.cymru.com TXT"
 
 class Host
-  attr_accessor :ip_address, :asn, :as_name
+  attr_accessor :ip_address, :asn, :as_name, :as_country
 
   def initialize(args)
     @ip_address = args[:address]
@@ -18,13 +18,24 @@ class Host
   end
 
   def set_asn
-    string = LOOKUP_PREFIX + self.reverse.address + LOOKUP_SUFFIX
-    self.asn =  `#{string}`.split("|")[0].strip
+    string = LOOKUP_PREFIX + self.reverse_address + LOOKUP_SUFFIX
+    self.asn =  "AS" + `#{string}`.slice(1..-2).split("|")[0].strip || "<no ASN>"
   end
 
   def set_as_name
     string = LOOKUP_PREFIX + @asn + AS_LOOKUP_SUFFIX
-    self.as_name = `#{string}`.split("|")[-1].strip
+    self.as_name = `#{string}`.split("|")[-1] || "<no AS name>"
+  end
+
+  def details_lookup
+    string = LOOKUP_PREFIX + self.reverse_address + LOOKUP_SUFFIX
+    as_lookup = `#{string}`.slice(1..-2).split("|")
+    self.asn = "AS" + as_lookup[0].strip || "<no ASN>"
+    self.as_country = as_lookup[2] || "<no country>"
+    string = LOOKUP_PREFIX + @asn + AS_LOOKUP_SUFFIX
+    #puts "#{string}"
+    #puts `#{string}`
+    self.as_name = `#{string}`.slice(1..-2).split("|")[-1] || "<no AS name>"    
   end
 end
 
@@ -32,13 +43,32 @@ class Record
   attr_accessor :host, :traffic_volume
 
   def initialize(args)
+    #puts args
     @host = args[:host]
-    @traffic_volume = args[:traffic] * Traffic_units[args[:units]]
+    #puts "#{args[:traffic]}  #{args[:units]}  #{Traffic_units[args[:units].to_sym]}"
+    #puts 19.6 * 1000000000
+    #puts Traffic_units[args[:units].to_sym]
+    #puts args[:traffic] * Traffic_units[args[:units].to_sym]
+    @traffic_volume = (args[:traffic].to_f * Traffic_units[args[:units].to_sym]).to_i
+    #@traffic_volume = 19.6 * 1000000000
   end
 
   def set_asn
     @host.set_asn
   end
+
+  def ip_address
+    self.host ? host.ip_address : "<nil>"
+  end
+
+  def asn
+    self.host ? host.asn : "<nil>"
+  end
+
+  def as_name
+    self.host ? host.as_name : "<nil>"
+  end
+
 
 end
 
@@ -78,5 +108,8 @@ lines.each do |line|
 end
 
 records.each do |record|
-  puts record.ip_address +"\t" + record.asn + "\t" + record.as_name
+  #record.host.set_asn
+  #record.host.set_as_name
+  record.host.details_lookup
+  puts record.ip_address + "\t" + record.asn + "\t" + record.as_name + record.traffic_volume.to_s
 end
